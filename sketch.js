f617ce3b7ad7dbac2520ce5844b0fc9741b88041
SaiGokul3109 <98755158+SaiGokul3109@users.noreply.github.com>
2022-02-26 15:28:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var student;
+
+function setup() {
+  createCanvas(400,400);
+//name="John";
+//num=9;
+//newarray=["item1",32,name,num];
+//console.log(newarray[1]);
+//console.log(newarray[2]);
+//console.log(newarray.length);
+student=new Student("Chris",14,8);
+student.display();
+
+
+}
+
+function draw() 
+{
+  background(30);
+  
+}
